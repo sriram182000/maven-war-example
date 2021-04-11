aa0841e0ba729cf666ab541e8520d98dd0eef325
Deekshith42 <63908044+Deekshith42@users.noreply.github.com>
2021-04-11 08:13:52 +0530

    commit 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello\nDeploy war file into Tomcat container using Jenkins automation by Deekshith_Maram");
+		get("/hello", (req, res) -> "Deployed war file successfully into Tomcat container using Jenkins automation by Deekshith");
 	}
 }
